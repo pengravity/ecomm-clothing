d81111bae03a6f728b44617f7231dbae72eddfcf
Andy <alipinski@mail.com>
2022-08-23 13:21:37 +0200

    refactoring to use createStructuredSelector in several components

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ import {
 import React from "react";
 import { connect } from "react-redux";
 import { setCurrentUser } from "./redux/user/user.actions";
+import { selectCurrentUser } from "./redux/user/user.selector";
+import { createStructuredSelector } from "reselect";
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
@@ -62,8 +64,8 @@ class App extends React.Component {
   }
 }
 
-const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUser,
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser,
 });
 
 const mapDispatchToProps = (dispatch) => ({
