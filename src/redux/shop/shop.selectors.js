2383d97b86c9695e1373f09f993aceae4c80286a
Andy <alipinski@mail.com>
2022-08-31 18:26:01 +0200

    displaying categories on individul pages

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -2,19 +2,16 @@ import memoize from "lodash.memoize";
 
 import { createSelector } from "reselect";
 
-const COLLECTION_ID_MAP = {
-  hats: 1,
-  shoes: 2,
-  jackets: 3,
-  womens: 4,
-  mens: 5,
-};
-
 const selectShop = (state) => state.shop;
 
 export const selectCollections = createSelector(
   [selectShop],
   (shop) => shop.collections
+);
+
+export const selectCollectionsForPreview = createSelector(
+  [selectCollections],
+  (collections) => Object.keys(collections).map((key) => collections[key])
 );
 
 export const selectCollection = memoize((collectionUrlParam) =>
